6dca0d000a883b8783433db4116cc3f2479106fb
Hazem-9 <mrhazem9@gmail.com>
2022-03-14 15:41:47 +0200

    Add annotation #2

--- Back-End/Student-System/src/main/java/com/spring/studnrt/studentsystem/Model/Student.java
+++ Back-End/Student-System/src/main/java/com/spring/studnrt/studentsystem/Model/Student.java
@@ -1,19 +1,45 @@
 package com.spring.studnrt.studentsystem.Model;
 
 import com.spring.studnrt.studentsystem.Model.enums.Gender;
+import jdk.jfr.Timestamp;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.xml.crypto.Data;
+import javax.persistence.*;
+import java.util.Date;
 
+
+@Entity(name = "stu dent")
 public class Student {
 
-    private String name;
-    private Gender gender;
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "name")
+    private String Name;
+
+    @Column(name = "Gender")
+    private Gender gender;
+
+    @Column(name = "age")
     private String Age ;
+
+    @Column(name = "phone")
     private String Phone ;
-    private String Addres;
-    private Data Createddate;
-    private Data  updateddate;
+
+    @Column(name = "address")
+    private String Address;
+
+    @CreationTimestamp
+    @Column(name = "created")
+    private Date CreatedDate;
+
+    @UpdateTimestamp
+    @Column(name = "updated")
+    private Date  updatedDate;
 
 
 
